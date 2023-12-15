b1aa161a15df8883ce50b0b30358b05c2db59ce7
luckys-lnz <lalucky22@gmail.com>
2023-12-15 21:26:53 +0000

    LocationDetails sitll need some work..not responsive

---
+++ src/components/LocationDetails.js.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const LocationDetails = () => {
+    return (
+            <article className='p-4 mt-9'>
+                <p>مرحبا، كيف حالك</p>
+
+            </article>
+
+  )
+}
